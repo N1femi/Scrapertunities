07e0b9ff058ee9f54dbc081c2595f8df5c049520
N1femi <niffybabs@gmail.com>
2026-03-02 21:57:30 -0500

    Used bs4 to scrape github and generically filter for applicable results

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-# Testing Testing Another Test
+from bs4 import BeautifulSoup
+import requests
 
-print("Nifemis Branch")
+response = requests.get("https://github.com/vanshb03/Summer2026-Internships")
+
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
+    results = soup.find_all("tbody") # We want the second one
+    tbody = results[1]
+    
+    rows = tbody.find_all("tr")
+    
+    for row in rows:
+        if "🔒" not in str(row): # Filters the locked ones
+            print(f"\n NEW ROW!!! --> {row} \n ")
+            
+    
+    
